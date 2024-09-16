ab3cccdb3ef951f1dc3df2d227ccffaa9c8282df
Haijun Si <sihaijun1@gmail.com>
2024-09-16 19:13:42 -0400

    no unused vars

--- pages/api/daily_habits/index.ts
+++ pages/api/daily_habits/index.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { sql } from '@vercel/postgres';
 import Habit from '@/app/models/Habit';
 import { getHabits } from '@/pages/api/habits';
-import { filter } from 'framer-motion/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { userId } = getAuth(req)
